2b15211b0f5737a4cf1bc60d323df85ff7896f96
Jas2365 <jastha6wdc@gmail.com>
2025-07-31 11:03:15 +0530

    demo ready

--- config/db.js
+++ config/db.js
@@ -5,9 +5,9 @@ export const ConnectDB = async () => {
     const conn = await mongoose
       .connect(process.env.MONGO_URI)
       .catch((err) => console.error(err));
-    // console.log(`MongoDB Connected host: ${conn.connection.host}`);
-    // console.log(`MongoDB Connected post: ${conn.connection.port}`);
-    // console.log(`MongoDB Connected DB: ${conn.connection.name}`);
+    console.log(`MongoDB Connected host: ${conn.connection.host}`);
+    console.log(`MongoDB Connected post: ${conn.connection.port}`);
+    console.log(`MongoDB Connected DB: ${conn.connection.name}`);
   } catch (error) {
     console.error(`Error ${error.message}`);
     process.exit(1);
--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const FRONTEND_URL = process.env.FRONTEND_URL;
 app.use(
   cors({
     origin: FRONTEND_URL,
-    credentials: true,
   })
 );
 app.use(express.json());
