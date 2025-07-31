82599a7098d0d5812d5f96b1d0b1bbdff2e3b1ab
Jas2365 <jastha6wdc@gmail.com>
2025-07-31 12:52:48 +0530

    async mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
 import app from "../server";
 
-app.use("/api/teachers", teacherRoutes);
-
 export default app;
--- server.js
+++ server.js
@@ -8,7 +8,15 @@ import { ConnectDB } from "./config/db.js";
 dotenv.config();
 
 const app = express();
-ConnectDB();
+
+async () => {
+  try {
+    await ConnectDB();
+    console.log("mongo db connected");
+  } catch (err) {
+    console.error("Mongo db failed", err);
+  }
+};
 const PORT = process.env.PORT || 2365;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
@@ -18,15 +26,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.json());
 app.use(
   cors({
     origin: FRONTEND_URL,
   })
 );
-app.use(express.json());
+
+app.use("/api/teachers", teacherRoutes);
 
 export default app;
-// app.listen(PORT, () => {
-//   console.log("the server is on! http://localhost:" + PORT);
-//   console.log("the front end origin is :", FRONTEND_URL);
-// });
